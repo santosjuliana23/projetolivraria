9184ffbd7a7c62c4296a3849cafc658b5a806b64
santosjuliana13 <a.pkskj@gmail.com>
2025-08-29 14:33:14 -0300

    feat: filtrando apenas as compras do usuário autenticado

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -43,6 +43,14 @@ class CompraSerializer(ModelSerializer):
         fields = ('id', 'usuario', 'status', 'total', 'itens')
         read_only_fields = ['id', 'usuario', 'status']
         
+class ItensCompraListSerializer(ModelSerializer):
+    livro = CharField(source='livro.titulo', read_only=True)
+
+    class Meta:
+        model = ItensCompra
+        fields = ('quantidade', 'livro')
+        depth = 1        
+        
 class CompraListSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
     itens = ItensCompraListSerializer(many=True, read_only=True)
@@ -52,10 +60,4 @@ class CompraListSerializer(ModelSerializer):
         fields = ('id', 'usuario', 'itens')
 
 
-class ItensCompraListSerializer(ModelSerializer):
-    livro = CharField(source='livro.titulo', read_only=True)
 
-    class Meta:
-        model = ItensCompra
-        fields = ('quantidade', 'livro')
-        depth = 1
