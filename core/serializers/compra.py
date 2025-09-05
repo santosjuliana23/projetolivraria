daa480f0ae7fec375db8e951927a2d696b4c63f5
santosjuliana13 <a.pkskj@gmail.com>
2025-09-05 14:06:06 -0300

    feat: validando a quantidade de itens na compra.

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField, CurrentUserDefault,HiddenField
+from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField, CurrentUserDefault, HiddenField, ValidationError
 
 from core.models import Compra, ItensCompra
 
@@ -7,7 +7,16 @@ class ItensCompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
         fields = ('livro', 'quantidade')
-        
+    
+    def validate_quantidade(self, quantidade):
+        if quantidade <= 0:
+            raise ValidationError('A quantidade deve ser maior do que zero.')
+        return quantidade
+    def validate(self, item):
+        if item['quantidade'] > item['livro'].quantidade:
+            raise ValidationError('Quantidade de itens maior do que a quantidade em estoque.')
+        return item
+    
 class CompraCreateUpdateSerializer(ModelSerializer):
     itens = ItensCompraCreateUpdateSerializer(many=True)
     HiddenField(default=CurrentUserDefault())
